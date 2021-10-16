a82b30a16ab66111a996c428af023927d1481ada
Christy Perozzi <christy.perozzi@gmail.com>
2021-10-16 13:39:47 -0400

    commit tree 52d990e35006e698b51f7497dd4927ae5cf8210c

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+/*
+const record = [
+    { year: "1971", result: "N/A"},
+    { year: "1970", result: "N/A"},
+    { year: "1969", result: "W"},
+    { year: "1968", result: "N/A"},
+    { year: "1967", result: "N/A"}
+];
+*/
 
+function superbowlWin(array){
+    let winObj = array.find(item => item.result === "W")
+    if (winObj){
+        return winObj.year;
+    }
+    else {
+        return undefined;
+    }
+};
+
+
+//console.log(superbowlWin(record));
